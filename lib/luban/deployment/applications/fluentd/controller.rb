714cec315aae8560bde4762601e7a0201b4702a3
Chi Man Lei <chi.lei@laxino.com>
2016-09-20 11:55:51 +0800

    Refactored the way of composing shell commands

--- lib/luban/deployment/applications/fluentd/controller.rb
+++ lib/luban/deployment/applications/fluentd/controller.rb
@@ -4,24 +4,16 @@ module Luban
       class Fluentd
         class Controller < Luban::Deployment::Application::Controller
           module Commands
-            def ruby_bin_path
-              @ruby_bin_path ||= package_bin_path('ruby')
-            end
-
-            def bundle_executable
-              @bundle_executable ||= ruby_bin_path.join('bundle')
-            end
-
             def process_pattern
               @process_pattern ||= "^supervisor:#{env_name}"
             end
 
             def start_command
-              @start_command ||= "#{bundle_executable} exec fluentd -c #{control_file_path} -d #{pid_file_path} -o #{log_file_path}"
+              @start_command ||= bundle_command("fluentd -c #{control_file_path} -d #{pid_file_path} -o #{log_file_path}")
             end
 
             def stop_command
-              @stop_command ||= "kill $(cat #{pid_file_path} 2>/dev/null)"
+              @stop_command ||= shell_command("kill $(cat #{pid_file_path} 2>/dev/null)")
             end
           end
 
